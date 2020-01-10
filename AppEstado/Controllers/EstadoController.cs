244a643e7ddd207b22bc1d02a082445e67144d8b
Danielia Oliveira <danielia.de@bol.com.br>
2020-01-10 18:03:24 -0300

    Correção

--- AppEstado/Controllers/EstadoController.cs
+++ AppEstado/Controllers/EstadoController.cs
@@ -1,5 +1,7 @@
 ﻿using AppEstado.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,60 +12,55 @@ using ActionResult = System.Web.Mvc.ActionResult;
 
 namespace AppEstado.Controllers
 {
-    [System.Web.Mvc.Route("api/[controller]")]
-    [ApiController]
+    ///[System.Web.Mvc.Route("api/[controller]")]
+    //[ApiController]
     public class EstadoController : Controller
     {
-        private IEstadoRepositorio _estadoRepositorio;
-
-        public EstadoController()
-        {
-            this._estadoRepositorio = new EstadoRepositorio(new EstadoContex());
-        }
-
-        // GET: /Estado/
+      
         public ActionResult GetTodosEstado()
         {
-           var results = from estado in _estadoRepositorio.GetEstado()
-                     select estado;
 
-            return View(results);
+            var estado = new RestClient("http://homologacao.service.mergefusao.agrohub.com.br/Services/CategoriaOficialService.svc");
+            var estados = new RestRequest("/ObterPorEstadoFazenda/2", DataFormat.Json);
+
+            var response = estado.Get(estados);
+
+            var objObterEstados = JsonConvert.DeserializeObject<dynamic>(response.Content);
+            //((Newtonsoft.Json.Linq.JProperty)((Newtonsoft.Json.Linq.JContainer)(JsonConvert.DeserializeObject<Object>(response.Content))).First).Value
+            //var objEstados = JsonConvert.DeserializeObject<Estado>(objObterEstados);
+            return View(objObterEstados);
+
+            // return View(response.Content);
+
+        }
+
+        //// GET: estado/Details/5
+        //public IActionResult Details(int? id)
+        //{
+
+            //if (id == null)
+            //{
+            //    return NotFound();
+            //}
 
            
+
+            // var estado = await _context.estado;
+            //    .FirstOrDefaultAsync(m => m.Id == id);
+            //if (estado == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //return View(estado)
+           
         }
-
-
-        }
-
 
     }
 
 
 
 
-////GET: /Estdo/Details/5
-//public ActionResult Details(int id)
-//{
-//    Estado estado = _estadoRepositorio.GetEstadoPorID(id);
-//    return View(estado);
-//}
-
-////// GET api/Estado/5
-////[HttpGet("{id}")]
-////public ActionResult GetById(int id)
-////{
-////    try
-////    {
-////        var empresa = await _repo.GetEmpresaAsyncById(id);
-////        var result = _mapper.Map<EmpresaDto>(empresa);
-////        return Ok(result);
-////    }
-////    catch (System.Exception)
-////    {
-////        return StatusCode(StatusCodes.Status500InternalServerError,
-////            "Ocorreu um erro no banco de dados");
-////    }
-
-
+     
 
 
